7c2910137cdb2f83ca41febc951a453ed0892117
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2023-09-22 15:18:05 -0300

    feat: relacion with Prisma

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,11 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from 'src/repositories/users.repository';
 import { UserInMemoryRepository } from 'src/in-memory/users.in-memory.repository';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [UsersController],
   providers: [
     UsersService,
+    PrismaService,
     { provide: UserRepository, useClass: UserInMemoryRepository },
   ],
 })
